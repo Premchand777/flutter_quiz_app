b58cc4b236b7fd7b61b8299fb9c02635fc997496
Premchand Kodali <kodalipremchand07@gmail.com>
2024-03-06 16:29:32 +0530

    Separated start screen as a custom stateless widget instead of directly defining on body argument of scaffold. Wrapped start screen custom widget around the wrapper i.e container as we are going to have same style and settings for next screens as well to apply common painting, positioning and sizing the coming widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -2,51 +2,26 @@
 // import flutter pkgs
 import 'package:flutter/material.dart';
 
+// import project pkgs
+import 'package:quiz_app/start_screen.dart';
+
 void main() {
   runApp(
     MaterialApp(
       title: 'quiz_app',
       home: Scaffold(
-        backgroundColor: Colors.indigo,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'lib/assets/images/quiz-logo.png',
-                width: 250,
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              const Text(
-                'Learn the Flutter the fun way!',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.normal
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(Colors.indigo.shade700),
-                  shadowColor: const MaterialStatePropertyAll(Colors.grey),
-                ),
-                child: const Text(
-                  'Start Quiz',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
+        // backgroundColor: Colors.indigo,
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.indigo.shade700,
+                Colors.indigo.shade900
+              ]
+            ),
           ),
-        ),
+          child: const StartScreen(),
+        )
       ),
     ),
   );
---
+++ lib/start_screen.dart
@@ -0,0 +1,51 @@
+
+// import flutter pkgs
+import 'package:flutter/material.dart';
+
+// start screen stateless custom widget
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'lib/assets/images/quiz-logo.png',
+            width: 250,
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const Text(
+            'Learn the Flutter the fun way!',
+            style: TextStyle(
+              fontWeight: FontWeight.normal,
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.indigo,
+              shadowColor: Colors.indigoAccent,
+            ),
+            child: const Text(
+              'Start Quiz',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
